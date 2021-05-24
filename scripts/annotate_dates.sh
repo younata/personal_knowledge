8d97ad59ee7445f70d4000b2d81c3dcaccf950f8
Rachel Brindle <you@subluminal.net>
2021-05-23 20:54:47 -0700

    Update the annotate_dates script to make that a link to the commit that last modified the file

--- scripts/annotate_dates.sh
+++ scripts/annotate_dates.sh
@@ -6,6 +6,7 @@ while IFS= read -r -d '' -u 9
 do
     if [[ "${REPLY}" =~ ^.+\.md$ ]]; then
         LAST_MODIFY_DATE=`git --no-pager log -n 1 --pretty=format:%ci "${REPLY}"`
-        printf "\n\nLast updated: ${LAST_MODIFY_DATE}" >> "${REPLY}"
+        LAST_HASH=`git --no-pager log -n 1 --pretty=format:%H "${REPLY}"`
+        printf "\n\nLast updated: [${LAST_MODIFY_DATE}](https://github.com/younata/personal_knowledge/commit/${LAST_HASH})" >> "${REPLY}"
     fi
 done 9< <( find src -type f -exec printf '%s\0' {} + )
